24998b384aad8625c66e29e17fab5ba64ccf58ce
Alexsey-VR <vityaz.iss@yandex.ru>
2026-01-25 06:11:34 +0000

    added user register page

--- public/index.php
+++ public/index.php
@@ -3,8 +3,15 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // As a parameter the base directory is used to contain a templates
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
@@ -12,17 +19,61 @@ $app->get('/', function ($request, $response) {
     return $response;
 });
 
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
+$usersFilePath = __DIR__ . '/../storage/users.json';
+
+$app->get('/users', function ($request, $response) use ($usersFilePath) {
+    $users = [];
+    if (file_exists($usersFilePath)) {
+        $jsonFileData = file_get_contents($usersFilePath);
+        $users = json_decode($jsonFileData, flags:JSON_OBJECT_AS_ARRAY);
+    }
+
+    $params = [
+        'users' => $users,
+        'term' => $request->getQueryParam('term')
+    ];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
+$app->post('/users', function ($request, $response) use ($usersFilePath) {
+    $user = $request->getParsedBodyParam('user');
+    $users = [];
+    if (file_exists($usersFilePath)) {
+        $jsonFileData = file_get_contents($usersFilePath);
+        $users = json_decode($jsonFileData, flags:JSON_OBJECT_AS_ARRAY);
+    }
+
+    $users[] = $user;
+    $jsonUsers = json_encode($users);
+    $type = gettype($jsonUsers);
+    file_put_contents(
+        $usersFilePath, $jsonUsers
+    );
+    
+    return $response->withRedirect('/users', 302)
+                    ->write("nickname{$users[-1]['nickname']}, email = {$users[-1]['email']} registered!");
 });
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
+});
+/*
+$app->get('/users/{id}', function ($request, $response, array $args) {
+    $params = ['id' => $args['id'], 'nickname' => "user-{$args['id']}" ];
+    // The specified path is considered relative to the base directory for the
+    // templates specified at the configuration stage.
+    // $this is available thanks to https://php.net/manual/ru/closure.bindto.php
+    // $this is a dependency container in Slim.
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+*/
+
+$app->get('/users/new', function ($request, $response) {
+    $params = [
+        'user' => ['nickname' => '', 'email' => '']
+    ];
+    return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 });
 
 $app->run();

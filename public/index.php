4464538eb5e921230838be17d9e5fad418f5ab4f
Alexsey-VR <vityaz.iss@yandex.ru>
2026-02-08 15:53:03 +0000

    added cookies for users

--- public/index.php
+++ public/index.php
@@ -28,18 +28,14 @@ $router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
-    return $response;
+    return $response->withRedirect('/users');
 })->setName('mainPage');
 
-$usersFilePath = __DIR__ . '/../storage/users.json';
-
-$app->get('/users', function ($request, $response) use ($usersFilePath, $router) {
+$app->get('/users', function ($request, $response) use ($router) {
     $messages = $this->get('flash')->getMessages();
-    $users = [];
-    if (file_exists($usersFilePath)) {
-        $jsonFileData = file_get_contents($usersFilePath);
-        $users = json_decode($jsonFileData, flags:JSON_OBJECT_AS_ARRAY);
-    }
+
+    $jsonUsers = $request->getCookieParam('users', json_encode([]));
+    $users = json_decode($jsonUsers, JSON_OBJECT_AS_ARRAY);
 
     $params = [
         'users' => $users,
@@ -47,31 +43,32 @@ $app->get('/users', function ($request, $response) use ($usersFilePath, $router)
         'router' => $router,
         'flash' => $messages
     ];
-    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+    return $this->get('renderer')->render(
+        $response->withHeader('set-cookie', "users={$jsonUsers}"),
+        'users/index.phtml',
+        $params);
 })->setName('usersGet');
 
 $app->post('/users', function ($request, $response) use ($usersFilePath, $router) {
     $user = $request->getParsedBodyParam('user');
-    $users = [];
 
-    if (file_exists($usersFilePath)) {
-        $jsonFileData = file_get_contents($usersFilePath);
-        $users = json_decode($jsonFileData, flags:JSON_OBJECT_AS_ARRAY);
-    }
+    $users = json_decode($request->getCookieParam('users', json_encode([])), JSON_OBJECT_AS_ARRAY);
+    $jsonUsers = json_encode($users);
 
     $validator = new Validator();
     $errors = $validator->validate($user);
     if (count($errors) === 0) {
-        $user['id'] = count($users) === 0 ? 0 : array_last($users)['id'] + 1;
+        $user['id'] = array_last($users ?? [])['id'] + 1;
         $users[] = $user;
         $jsonUsers = json_encode($users);
-        file_put_contents(
-            $usersFilePath,
-            $jsonUsers
-        );
+        
         $this->get('flash')->addMessage('success', 'User was added successfully');
         
-        return $response->withRedirect($router->urlFor('usersGet'), 302);
+        return $response->withHeader('set-cookie', "users={$jsonUsers}")
+                        ->withRedirect($router->urlFor('usersGet'), 302);
+    } else {
+        return $response->getBody()->write("$errors[-1]")
+                        ->withRedirect($router->urlFor('usersGet'), 404);
     }
 
     $this->get('flash')->addMessage('error', 'Form data has an error');
@@ -86,14 +83,11 @@ $app->post('/users', function ($request, $response) use ($usersFilePath, $router
         $params);
 })->setName('usersPost');
 
-$app->get('/courses/{id}', function ($request, $response, array $args) {
-    $id = $args['id'];
-    return $response->write("Course id: {$id}");
-})->setName('coursesIdGet');
-
 $app->get('/users/{id}/edit', function ($request, $response, array $args) use ($usersFilePath) {
     $id = $args['id'];
-    $users = json_decode(file_get_contents($usersFilePath), JSON_OBJECT_AS_ARRAY);
+
+    $users = json_decode($request->getCookieParam('users', json_encode([])), JSON_OBJECT_AS_ARRAY);
+
     $message = $this->get('flash')->getMessages();
     $user = $users[$id];
     if (array_key_exists($id, $users)) {
@@ -102,6 +96,7 @@ $app->get('/users/{id}/edit', function ($request, $response, array $args) use ($
             'flash' => $message,
             'errors' => []
         ];
+        $jsonUsers = json_encode($users);
         return $this->get('renderer')->render(
             $response,
             'users/patch.phtml',
@@ -123,20 +118,29 @@ $app->get('/users/new', function ($request, $response) use ($router) {
 
 $app->patch('/users/{id}', function ($request, $response, array $args) use ($router, $usersFilePath) {
     $id = $args['id'];
-    $users = json_decode(file_get_contents($usersFilePath), JSON_OBJECT_AS_ARRAY);
+
+    $users = json_decode($request->getCookieParam('users', json_encode([])), JSON_OBJECT_AS_ARRAY);
+
     $user = $request->getParsedBodyParam('user');
     $validator = new Validator();
     $errors = $validator->validate($user);
     if (count($errors) === 0) {
-        $users[$id]['nickname'] = $user['nickname'];
-        $users[$id]['email'] = $user['email'];
-        file_put_contents(
-            $usersFilePath,
-            json_encode($users)
+        $updatedUsers = array_filter(
+            $users,
+            function ($user) use ($id) {
+                return $user['id'] != $id;
+            }
         );
+
+        $updatedUsers[$id]['id'] = $id;
+        $updatedUsers[$id]['nickname'] = $user['nickname'];
+        $updatedUsers[$id]['email'] = $user['email'];
+
+        $jsonUsers = json_encode($updatedUsers);
+
         $this->get('flash')->addMessage('success', 'user data successfully updated');
-        return $response->withRedirect(
-            $router->urlFor('userEdit', ['id' => $id])
+        return $response->withHeader('set-cookie', "users={$jsonUsers}; MAX-AGE=1")
+                        ->withRedirect($router->urlFor('usersGet')
         );
     } else {
         $params = [
@@ -151,9 +155,11 @@ $app->patch('/users/{id}', function ($request, $response, array $args) use ($rou
     }
 });
 
-$app->get('/users/{id}/delete', function ($requeset, $response, array $args) use ($usersFilePath) {
+$app->get('/users/{id}/delete', function ($request, $response, array $args) use ($usersFilePath) {
     $id = $args['id'];
-    $users = json_decode(file_get_contents($usersFilePath), JSON_OBJECT_AS_ARRAY);
+
+    $jsonUsers = $request->getCookieParam('users', json_encode([]));
+    $users = json_decode($jsonUsers, JSON_OBJECT_AS_ARRAY);
     if (array_key_exists($id, $users)) {
         $params = [
             'user' => $users[$id],
@@ -173,8 +179,11 @@ $app->get('/users/{id}/delete', function ($requeset, $response, array $args) use
 
 $app->delete('/users/{id}', function ($request, $response, array $args) use ($usersFilePath, $router) {
     $id = $args['id'];
-    $users = json_decode(file_get_contents($usersFilePath), JSON_OBJECT_AS_ARRAY);
+
+    $users = json_decode($request->getCookieParam('users', json_encode([])), JSON_OBJECT_AS_ARRAY);
+
     $user = $request->getParsedBodyParam('user');
+    $url = $router->urlFor('usersGet');
     if ($user['remove'] === 'true') {
         $updatedUsers = array_filter(
             $users,
@@ -182,30 +191,32 @@ $app->delete('/users/{id}', function ($request, $response, array $args) use ($us
                 return $user['id'] != $id;
             }
         );
-        file_put_contents(
-            $usersFilePath,
-            json_encode($updatedUsers)
-        );
-        $this->get('flash')->addMessage('success', "User with ID: {$id} has been deleted");
-    }
 
-    $url = $router->urlFor('usersGet');
+        $jsonUsers = json_encode($updatedUsers);
+        $this->get('flash')->addMessage('success', "User has been deleted");
+        return $response->withHeader('set-cookie', "users={$jsonUsers}; MAX-AGE=1")
+                        ->withRedirect($url); 
+    }
     return $response->withRedirect($url);
 });
 
 $app->get('/users/{id}', function ($request, $response, array $args) use ($usersFilePath) {
-    $fileContent = json_decode(file_get_contents($usersFilePath), JSON_OBJECT_AS_ARRAY);
+    $users = json_decode($request->getCookieParam('users', json_encode([])), JSON_OBJECT_AS_ARRAY);
     $userId = $args['id'];
-    if (isset($fileContent[$userId])) {
+    if (isset($users[$userId])) {
         $params = [
             'id' => $args['id'],
-            'nickname' => "{$fileContent[$args['id']]['nickname']}"
+            'nickname' => "{$users[$args['id']]['nickname']}"
         ];
         // The specified path is considered relative to the base directory for the
         // templates specified at the configuration stage.
         // $this is available thanks to https://php.net/manual/ru/closure.bindto.php
         // $this is a dependency container in Slim.
-        return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+        return $this->get('renderer')->render(
+            $response,
+            'users/show.phtml',
+            $params
+        );
     } else {
         // The specified path is considered relative to the base directory for the
         // templates specified at the configuration stage.
